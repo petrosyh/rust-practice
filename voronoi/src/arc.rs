3b8cb32fc4e76fd9887ba2270df98b0a5c2545ce
yonghyun kim <yonghyun.kim@sf.snu.ac.kr>
2018-12-15 18:26:37 +0900

    WIP

--- voronoi/src/arc.rs
+++ voronoi/src/arc.rs
@@ -5,18 +5,21 @@ use std::fmt;
 
 // Arc for Circle Event.
 pub struct Arc{
-    pub leftend: Point,
-    pub rightend: Point,
     pub site: Point, // site point of this arc
 }
 
+// a Node for BeachLine
 pub struct ArcTriple {
     // arcs end points must same.
     pub left_arc: Arc,
+    pub left_breakpt: Point,
     pub ceter_arc: Arc,
+    pub right_breakpt: Point,
     pub right_arc: Arc,
-    pub event_point: Point,
+    pub event_point: Point, // point for Circle Event
 }
+
+
 
 impl fmt::Debug for ArcTriple {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
